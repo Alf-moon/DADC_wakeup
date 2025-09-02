86bb39094c7cfd19f0d2505bc2d474ec639cddbe
nvidia <szou1@jaguarlandrover.com>
2025-09-02 12:48:31 +0800

    Bug fix again

--- DADC_Wakeup_RawFrame.py
+++ DADC_Wakeup_RawFrame.py
@@ -37,11 +37,11 @@ def send_wakeup_frame(interface, dst_mac, src_mac, eth_type, payload):
             eth_type.to_bytes(2, byteorder='big') + \
             someip_frame
 
-# 创建原始套接字
-s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
-s.bind((interface, 0))
-s.send(frame)
-s.close()
+    # 创建原始套接字
+    s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+    s.bind((interface, 0))
+    s.send(frame)
+    s.close()
 
 if __name__ == "__main__":
     interface = "enp0s31f6"  # 替换为你的网卡名
